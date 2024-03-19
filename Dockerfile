17afa75a08b7aa8b086fd32559e284ac74b9c8f0
Mariusz Kogen <k0gen@pm.me>
2024-03-19 11:23:33 +0100

    Update to latest and gratest - version bump - upstream image update - build and release workflow update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM gitea/gitea:1.21.2
+FROM gitea/gitea:1.21.8
 
 RUN apk add --no-cache tini curl yq; \
     rm -f /var/cache/apk/*
--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -38,4 +38,4 @@ export const migration: T.ExpectedExports.migration = compat.migrations
         throw new Error("Downgrade not supported");
       },
     },
-  }, "1.21.2");
+  }, "1.21.8");
